804bc8bd98d1c6af1ba11a86064ee203648c2f4d
SURYASH SAHU <suryashsahu600@gmail.com>
2025-11-04 05:21:53 +0000

    Strings

---
+++ 01_basics/learnstring.js
@@ -0,0 +1,23 @@
+let name="Suryash_Sahu";
+console.log(typeof name );
+//object type string
+var stuname = new String("Suryah_Sahu");
+console.log(typeof stuname);
+console.log(stuname);
+let age = 23 ;
+let chick= 23>22;
+console.log(chick);
+console.log(name+" "+ age +" "+ chick);// It is not Write prectice
+//moder time we use `` to asign value in string
+console.log(`Student name ${name} this age ${age} and its age greater then jiya ${chick}`)
+console.log(stuname.length);
+console.log(stuname);
+console.log(__proto__);
+console.log(stuname.toUpperCase());
+console.log(stuname.charAt(0))
+console.log(stuname.at(5));
+console.log(stuname.anchor(linkdin="https://www.linkedin.com/in/suryash-sahu-0012ec211028/"))
+console.log(stuname.charCodeAt());// "S"ASCII number nikle ga 
+
+
+
