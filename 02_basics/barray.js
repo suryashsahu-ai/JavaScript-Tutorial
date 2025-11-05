82238065747abf5376b5c30fb0a88b2c876158b3
SURYASH SAHU <suryashsahu600@gmail.com>
2025-11-05 11:04:22 +0000

    #Array Start

---
+++ 02_basics/barray.js
@@ -0,0 +1,16 @@
+const frist=['Iron', 'thor','halk','captain'];
+const nums=[0,1,2,3,4,5,];
+/*Array : Array is a set of collection which use to store multiple type datatype object function method etc in javascript 
+ what Speacial in JS
+ In js Array store multiple type of data
+ primitive or non-primitive datatype in single  Array container
+ JavaScript Copy-operation create Shollow copy
+ Shollow Copy: It is a object Copy whose properties share the same references
+ Matlab Same reffrence hutha hai agar hum uske copy me update karege toh uska real object me bhi changes huge because array have non-primitive type
+*/
+console.log(nums[3]);
+
+
+
+
+
